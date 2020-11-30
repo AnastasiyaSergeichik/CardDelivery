83a005854db1a8845c02b4b271ad5e1144280c98
Sergey Sergeychik <sergey.sergeychik@gmail.com>
2020-11-30 16:22:59 +0300

    Корректировка тестов

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,78 +1,85 @@
 package ru.netology;
 
 import com.codeborne.selenide.SelenideElement;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import org.openqa.selenium.Keys;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
+
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
-
 public class CardDeliveryTest {
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-    }
+    SelenideElement form = $(".form");
 
     @BeforeEach
-    void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-    }
+    void setup() {
+        open("http://localhost:9999");
+        form = $("[action]");
 
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
     }
 
     @Test
     void shouldSubmitRequest() {
-        open("http://localhost:9999");
-        SelenideElement form = $(".form");
+        String date = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         form.$("[data-test-id=city] input").setValue("Санкт-Петербург");
-        form.$("[data-test-id=date] input").setValue("");
+        form.$(".input__icon").click();
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").sendKeys(date);
         form.$("[data-test-id=name] input").setValue("Иван Петров");
         form.$("[data-test-id=phone] input").setValue("+79200000000");
         form.$("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-        $(withText("Успешно")).waitUntil(visible, 15000);
+        $("[data-test-id=notification]").waitUntil(visible, 15000).shouldHave(exactText("Успешно! Встреча успешно забронирована на "
+                + date));
+    }
+
+    //Задача 2 (НЕОБЯЗАТЕЛЬНАЯ)
+
+    @Test
+    void shouldSubmitRequestList() {
+        String reservationDate = LocalDate.now().plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        form.$("[data-test-id=city] input").setValue("Са");
+        $$(".menu-item").find(exactText("Санкт-Петербург")).click();
+        form.$(".input__icon").click();
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").sendKeys(reservationDate);
+        form.$("[data-test-id=name] input").setValue("Иван Петров");
+        form.$("[data-test-id=phone] input").setValue("+79200000000");
+        form.$("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=notification]").waitUntil(visible, 15000).shouldHave(exactText("Успешно! Встреча успешно забронирована на "
+                + reservationDate));
     }
 
     @Test
-    void shouldSubmitRequestDwopDownList() {
-        Calendar c = Calendar.getInstance();
-        c.add(Calendar.DATE, 7);
-        DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
-        String reservationDate = (df.format(c.getTime()));
-        open("http://localhost:9999");
-        SelenideElement form = $(".form");
+    void shouldSubmitRequestCalendar() {
         form.$("[data-test-id=city] input").setValue("Са");
-        $$(".menu-item__control").find(exactText("Санкт-Петербург")).click();
+        $$(".menu-item").find(exactText("Санкт-Петербург")).click();
         form.$(".input__icon").click();
         $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        form.$("[data-test-id=date] input").setValue(reservationDate);
+        LocalDate date = LocalDate.now();
+        LocalDate meetDate = date.plusDays(7);
+        int currentMonth = date.getMonthValue();
+        int meetMonth = meetDate.getMonthValue();
+        int meetDay = meetDate.getDayOfMonth();
+        if (meetMonth != currentMonth) {
+            $("[class='popup__container'] [data-step='1']").click();
+        }
+        $(byText(String.valueOf(meetDay))).click();
         form.$("[data-test-id=name] input").setValue("Иван Петров");
         form.$("[data-test-id=phone] input").setValue("+79200000000");
         form.$("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-        $(withText("Успешно")).waitUntil(visible, 15000);
+        $(withText("Успешно!")).waitUntil(visible, 15000);
+
     }
 }
+
